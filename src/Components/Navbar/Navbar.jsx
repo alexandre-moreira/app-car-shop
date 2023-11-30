1247aa41429c3009b5b0b8bf2bb5935ce71e3464
Alexandre Moreira <4lexandre.moreira@gmail.com>
2023-11-30 16:11:20 -0300

    feat: Function action navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Navbar.css'
 
 import logo from '../../Assets/car_logo.png';
@@ -7,34 +7,42 @@ import { IoIosCloseCircle } from "react-icons/io";
 import { TbGridDots } from "react-icons/tb";
 
 const Navbar = () => {
+  const [navbar, setNavbar] = useState('navbar');
+
+  const showNavbar = () => {
+    setNavbar('navbar showNavbar');
+  }
+
+  const removeNavbar = () => {
+    setNavbar('navbar');
+  }
   return (
     <div className='header'>
       <div className="logoDiv">
         <img src={logo} alt="Logo Imagem" className='logo' />
       </div>
-      <div className="navbar">
+      <div className={navbar}>
         <ul className='menu'>
-          <li className="listItem">
+          <li onClick={removeNavbar} className="listItem">
             <a href="/" className='link'>Used Cars</a>
           </li>
-          <li className="listItem">
+          <li onClick={removeNavbar} className="listItem">
             <a href="/" className='link'>New Cars</a>
           </li>
-          <li className="listItem">
+          <li onClick={removeNavbar} className="listItem">
             <a href="/" className='link'>Auctions</a>
           </li>
-          <li className="listItem">
+          <li onClick={removeNavbar} className="listItem">
             <a href="/" className='link'>Sell</a>
           </li>
         </ul>
 
-        <IoIosCloseCircle className='icon closeIcon'/>
+        <IoIosCloseCircle className='icon closeIcon' onClick={removeNavbar}/>
       </div>
       <div className="singUp flex">
         <div className="text">Sing Up</div>
-        <TbGridDots className='toggleNavbarIcon'/>
+        <TbGridDots className='toggleNavbarIcon' onClick={showNavbar}/>
       </div>
-     
     </div>
   )
 }
